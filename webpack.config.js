3b42a9aae0836662e82027969470ad155f43419a
Pernille Klevstuen <peklevstuen@gmail.com>
2019-10-03 21:24:35 +0200

    :pencil2: Fixing typos in webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -5,33 +5,33 @@ module.exports = {
   module: {
     rules: [
       {
-		test: /\.js$/,
-		exclude: /node_modules/,
-		use: {
-			loader: 'babel-loader',
-			options: {
-				presets: ['@babel/preset-env'],
-			},
-		},
-	  }, 
-	  {
-		test: /\.scss$/,
-		use: [
-			{ loader: 'style_loader' },
-			{ loader: 'css-loader' },
-			{ loader: 'sass-loader' }
-		]
-	  },
-	  {
-		  test: /\.tsx?$/m
-		  use: 'ts-loader',
-		  exclude: /node_modules/,
-	  }
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: "style_loader" },
+          { loader: "css-loader" },
+          { loader: "sass-loader" }
+        ]
+      },
+      {
+        test: /\.tsx?$/m,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
     ]
   },
   resolve: {
-	  extentions: ['.tsx', '.ts', '.js']
-  }
+    extensions: [".tsx", ".ts", ".js"]
+  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
