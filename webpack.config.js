e6bb37f9a08c85cbe43ab2746560e2593d92e281
Pernille Klevstuen <peklevstuen@gmail.com>
2019-10-03 21:02:44 +0200

    :wrench: Added webpack and babel, created global css-files

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+		test: /\.js$/,
+		exclude: /node_modules/,
+		use: {
+			loader: 'babel-loader',
+			options: {
+				presets: ['@babel/preset-env'],
+			},
+		},
+	  }, 
+	  {
+		test: /\.scss$/,
+		use: [
+			{ loader: 'style_loader' },
+			{ loader: 'css-loader' },
+			{ loader: 'sass-loader' }
+		]
+	  },
+	  {
+		  test: /\.tsx?$/m
+		  use: 'ts-loader',
+		  exclude: /node_modules/,
+	  }
+    ]
+  },
+  resolve: {
+	  extentions: ['.tsx', '.ts', '.js']
+  }
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  }
+};
