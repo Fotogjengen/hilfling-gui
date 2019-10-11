c225473e97ef459582a426ca02168007d859baaf
Sindre Sivertsen <sindrejohan1@gmail.com>
2019-10-11 20:28:40 +0200

    :ok_hand: fixed comments from code review

--- src/components/CardTitle/CardTitle.tsx
+++ src/components/CardTitle/CardTitle.tsx
@@ -16,19 +16,3 @@ export const CardTitle: FC<Props> = ({
 );
 
 export default CardTitle;
-
-// return (
-//     <button
-//       className={
-//         flat
-//           ? cx(styles.flatCardTitle, className)
-//           : cx(styles.button, styles[size], dark && styles.dark, className)
-//       }
-//       type={submit ? 'submit' : 'button'}
-//       {...rest}
-//     >
-//       <LoadingIndicator small margin={0} loading={pending} />
-//       {pending && <span className={styles.loading}>Laster</span>}
-//       {!pending && children}
-//     </button>
-//   );
