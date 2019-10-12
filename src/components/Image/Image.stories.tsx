ba1f5425de2af62b1319c46a9abfe2be9970aa94
Pernille Klevstuen <peklevstuen@gmail.com>
2019-10-13 00:10:48 +0200

    :lipstick: Added Image component

---
+++ src/components/Image/Image.stories.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { withInfo } from "@storybook/addon-info";
+import Image from "./Image";
+
+storiesOf("Image", module)
+  .addDecorator(withInfo({ inline: true }))
+  .add("Image", () => (
+    <Image
+      src={
+        "https://pbs.twimg.com/profile_images/3572978953/8c522d3ea384cd42e46a4a2498300c35_400x400.jpeg"
+      }
+      alt={"image"}
+    />
+  ));
