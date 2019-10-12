e0fcbc0f9cb014e4b945de6618dc23f2aed5ec3e
Caroline Sandsbråten <carolinesandsbraten@gmail.com>
2019-10-12 17:10:30 +0200

    :bug: fix too many tabs problem

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,32 +3,32 @@ import cx from "classnames";
 import styles from "./Button.css";
 
 interface Props {
-	/** Button label */
-	label: string;
-	/** Primary button styling */
-	primary?: boolean;
-	/** Danger button styling (red button) */
-	warning?: boolean;
-	/** Not clickable button */
-	disabled?: boolean;
-	/** Is it a submit button? */
-	submit?: boolean;
+  /** Button label */
+  label: string;
+  /** Primary button styling */
+  primary?: boolean;
+  /** Danger button styling (red button) */
+  warning?: boolean;
+  /** Not clickable button */
+  disabled?: boolean;
+  /** Is it a submit button? */
+  submit?: boolean;
 }
 
 export const Button: FC<Props> = ({
-	label,
-	primary,
-	warning,
-	disabled,
-	submit,
-	...rest
+  label,
+  primary,
+  warning,
+  disabled,
+  submit,
+  ...rest
 }: Props) => {
-	const buttonClass = cx(styles.Button, styles.warning, styles.disabled);
-	return (
-		<button className={buttonClass} {...rest}>
-			{label}
-		</button>
-	);
+  const buttonClass = cx(styles.Button, styles.warning, styles.disabled);
+  return (
+    <button className={buttonClass} {...rest}>
+      {label}
+    </button>
+  );
 };
 
 export default Button;
--- src/components/icons/CheckedCheckbox.tsx
+++ src/components/icons/CheckedCheckbox.tsx
@@ -1,20 +1,20 @@
 import React, { FC } from "react";
 
 const CheckedCheckboxSvg: FC<{}> = () => (
-	<svg
-		width="18"
-		height="18"
-		viewBox="0 0 18 18"
-		fill="none"
-		xmlns="http://www.w3.org/2000/svg"
-	>
-		<path
-			fillRule="evenodd"
-			clipRule="evenodd"
-			d="M16 0H2C0.9 0 0 0.9 0 2V16C0 17.1 0.9 18 2 18H16C17.1 18 18 17.1 18 16V2C18 0.9 17.1 0 16 0ZM7 14L2 8.71154L3.4 7.23077L7 11.0385L14.6 3L16 4.48077L7 14Z"
-			fill="#B42025"
-		/>
-	</svg>
+  <svg
+    width="18"
+    height="18"
+    viewBox="0 0 18 18"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M16 0H2C0.9 0 0 0.9 0 2V16C0 17.1 0.9 18 2 18H16C17.1 18 18 17.1 18 16V2C18 0.9 17.1 0 16 0ZM7 14L2 8.71154L3.4 7.23077L7 11.0385L14.6 3L16 4.48077L7 14Z"
+      fill="#B42025"
+    />
+  </svg>
 );
 
 export default CheckedCheckboxSvg;
