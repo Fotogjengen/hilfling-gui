19241efbfd19b7842dd9fd8c10314b4485b7684d
Pernille Klevstuen <peklevstuen@gmail.com>
2019-10-16 21:58:25 +0200

    :package: Exporting all components in index file

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import "./styles/globals.css";
 import "./styles/utilities.module.css";
 
 export { default as Button } from "./components/Button";
-export { default as Checkbox } from "./components/Checkbox";
+export { default as Card } from "./components/Card";
+export { default as CardPreamble } from "./components/CardPreamble";
 export { default as CardTitle } from "./components/CardTitle";
+export { default as Checkbox } from "./components/Checkbox";
+export { default as DotDivider } from "./components/DotDivider";
+export { default as Image } from "./components/Image";
+export { default as Input } from "./components/Input";
 export { default as RadioButton } from "./components/RadioButton";
