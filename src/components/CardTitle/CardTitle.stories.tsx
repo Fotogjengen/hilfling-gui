049228588ca130e9a50e9db91611985e8101d0ad
Pernille Klevstuen <peklevstuen@gmail.com>
2019-11-06 21:17:03 +0100

    :twisted_rightwards_arrows: Fetch newest code into branch

--- src/components/CardTitle/CardTitle.stories.tsx
+++ src/components/CardTitle/CardTitle.stories.tsx
@@ -6,6 +6,4 @@ import CardTitle from ".";
 storiesOf("CardTitle", module)
   .addDecorator(withInfo({ inline: true }))
   .add("Normal", () => <CardTitle title={"Beste tittelen"} />)
-  .add("Capitalized", () => (
-    <CardTitle title={"tittel med store bokstaver"} capitalized={true} />
-  ));
+  .add("Capitalized", () => <CardTitle title={"tittel med store bokstaver"} />);
