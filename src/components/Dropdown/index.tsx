6b5bda6715d0ac8e7980d6cf2fb70345e273cab2
Sindre Sivertsen <sindrejohan1@gmail.com>
2020-01-21 18:55:12 +0100

    :ok_hand: fixed review requests

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,21 +1,29 @@
 import React, { FC, useRef } from "react";
 import DropDownArrow from "../icons/DropDownArrow";
 import styles from "./Dropdown.module.css";
+import cx from "classnames";
 
 interface Props {
   options: Array<string>;
   whenSelected: () => void;
+  className?: any;
 }
 
-const Dropdown: FC<Props> = ({ options, whenSelected }: Props) => {
+const Dropdown: FC<Props> = ({
+  options,
+  whenSelected,
+  className,
+  ...rest
+}: Props) => {
   const optionsList = options.map(element => (
     <option onSelect={whenSelected} value={element}>
       {element}
     </option>
   ));
+  const selectClass = cx(className);
   return (
     <div>
-      <select className={styles.select} onChange={whenSelected}>
+      <select className={selectClass} onChange={whenSelected} {...rest}>
         {optionsList}
       </select>
       <div className={styles.arrow}></div>
