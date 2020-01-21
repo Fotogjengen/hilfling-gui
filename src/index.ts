93999888bdfaacb3d0c8ba9f0c8ea421d3c7ef5f
Pernille Klevstuen <peklevstuen@gmail.com>
2020-01-21 19:00:44 +0100

    :ambulance: Exported Footer

--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,4 @@ export { default as Logo } from "./components/Logo";
 export { default as Hamburger } from "./components/Hamburger";
 export { default as HeaderLink } from "./components/HeaderLink";
 export { default as Header } from "./components/Header";
+export { default as Footer } from "./components/Footer";
