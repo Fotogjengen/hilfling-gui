ebdfb5e0f940ac6f50044559d3cb407835013d32
Sindre Sivertsen <sindrejohan1@gmail.com>
2020-01-21 21:04:37 +0100

    :feature: made component. Added color-variable for headers

---
+++ src/components/ChildPageHeader/index.tsx
@@ -0,0 +1,70 @@
+import React, { FC } from "react";
+import cx from "classnames";
+import styles from "./ChildPageHeader.module.css";
+import { DefaultProps } from "../../types";
+
+interface Props extends DefaultProps {
+  title: string;
+}
+
+const ChildPageHeader: FC<Props> = ({ title, className, ...rest }: Props) => {
+  const style = cx(styles.header, className);
+  return (
+    <div>
+      <h1 className={style}>{title}</h1>
+      <div className={styles.underline}></div>
+    </div>
+  );
+};
+
+interface Props extends DefaultProps {
+  /** Content inside */
+  children?: any;
+  /** Primary button styling */
+  primary?: boolean;
+  /** Danger button styling (red button) */
+  warning?: boolean;
+  /** Called when a button is clicked */
+  onClick?: () => void;
+  /** Not clickable button */
+  disabled?: boolean;
+  /** Is it a submit button? */
+  submit?: boolean;
+}
+
+const Button: FC<Props> = ({
+  children,
+  primary,
+  warning,
+  onClick,
+  disabled = false,
+  submit,
+  className,
+  ...rest
+}: Props) => {
+  const buttonClass = cx(
+    styles.Button,
+    { [styles.primary]: primary },
+    { [styles.warning]: warning },
+    {
+      [styles.disabled]: disabled && !onClick,
+    },
+    className,
+  );
+  const handleClick = () => {
+    if (!onClick) return;
+    onClick();
+  };
+  return (
+    <button
+      className={buttonClass}
+      onClick={handleClick}
+      disabled={disabled}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default ChildPageHeader;
