102d93349a48a8a7719b7da4e516cfe5233c87ff
Pernille Klevstuen <peklevstuen@gmail.com>
2020-03-07 17:55:15 +0100

    :sparkles: ImaageCard component now exists

---
+++ src/components/Card/ImageCard.tsx
@@ -0,0 +1,53 @@
+import React, { FC } from "react";
+import Card from "../Card/.";
+import { DefaultProps } from "../../types";
+import { Props as CardProps } from "./index";
+import styles from "./ImageCard.module.css";
+import cx from "classnames";
+
+type TOP = "top";
+type LEFT = "left";
+
+type ImagePlacement = TOP | LEFT;
+
+interface Props extends DefaultProps, CardProps {
+  placement: ImagePlacement;
+  image: string;
+  children?: any;
+}
+
+const ImageCard: FC<Props> = ({
+  placement,
+  image,
+  type,
+  rounded,
+  children,
+}: Props) => {
+  return (
+    <div
+      className={cx({
+        [styles.top]: placement === "top",
+        [styles.left]: placement === "left",
+      })}
+    >
+      <img
+        className={cx({
+          [styles.topsize]: placement === "top",
+          [styles.leftsize]: placement === "left",
+        })}
+        src={image}
+      />
+
+      <Card
+        shadow={false}
+        type={type ? type : undefined}
+        rounded={rounded ? rounded : undefined}
+        className={styles.card}
+      >
+        <div>{children}</div>
+      </Card>
+    </div>
+  );
+};
+
+export default ImageCard;
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -4,13 +4,15 @@ import styles from "./Card.module.css";
 import { DefaultProps, EventType } from "../../types";
 
 
-interface Props extends DefaultProps {
+export interface Props extends DefaultProps {
   /** Children components */
   children?: any;
   /** Specifies type of event card */
   type?: EventType;
   /** Rounded corners */
   rounded?: boolean;
+  /** Shadow */
+  shadow?: boolean;
 }
 
 const Card: FC<Props> = ({
@@ -18,15 +20,20 @@ const Card: FC<Props> = ({
   type,
   rounded = false,
   className,
+  shadow = true,
+  ...rest
 }: Props) => {
   console.log(type);
   return (
     <div
-      className={
-        type
-          ? cx(styles[type], styles.card, className)
-          : cx(rounded && styles.rounded, styles.card, className)
-      }
+      className={cx(
+        {
+          [styles.shadow]: shadow,
+        },
+        styles.card,
+        className,
+        type ? cx(styles[type]) : cx(rounded && styles.rounded),
+      )}
     >
       {children}
     </div>
