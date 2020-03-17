1a0800248a898ddda7817ece785892bde7ad3262
Sindre Sivertsen <sindrejohan1@gmail.com>
2020-03-17 19:07:51 +0100

    1.1.16

--- src/components/RadioButton/index.tsx
+++ src/components/RadioButton/index.tsx
@@ -3,10 +3,12 @@ import { RadioButtonSvg } from "../icons";
 import styles from "./RadioButton.module.css";
 import cx from "classnames";
 import { DefaultProps } from "../../types";
+import Facebook from "../icons/Facebook";
 
 interface Props extends DefaultProps {
   /** True if radiobutton is checked */
-  checked?: boolean;
+  value: boolean;
+  defaultValue?: boolean;
   /** True if not able to change component state */
   notChangeable?: boolean;
   label?: string;
@@ -14,17 +16,18 @@ interface Props extends DefaultProps {
 }
 
 const RadioButton: FC<Props> = ({
-  checked = false,
+  value,
+  defaultValue = false,
   notChangeable,
   className,
   label,
   inputRef,
   ...rest
 }: Props) => {
-  const [value, setValue] = useState<boolean>(checked);
+  const [checked, setChecked] = useState<boolean>(value);
   const handleClick = () => {
     if (notChangeable) return;
-    setValue(!value);
+    setChecked(!checked);
   };
   return (
     <div className={styles.container}>
