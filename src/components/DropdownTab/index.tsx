4f6f694c935cc27d4f690b05c662220fe16aff8a
Taheera Ahmed <60318348+taheeraahmed@users.noreply.github.com>
2020-11-03 22:01:34 +0100

    FS-99: Feature/taheera/dropdown tab (#43)
    * work in progess
    
    * fix hide show
    
    * Laget en dropdowntab og lagt til ikoner
    
    * Index til dropdowntab
    
    * OverflowMenu og DropDownArrow Ikon
    
    * Deleting files from ContentTab
    
    * Overflow components for ThreeDotsMenu
    
    * Pencil og GarbageCan ikon - size optional
    
    * Endringer i Overflow (både index, css og tsx)
    
    * White space endringer i ContentTab og Dropdowntab
    
    * Endring i Overflow for å få riktig css
    
    * Endring av css i Overflow
    
    * Overflow - CSS endring
    
    * ThreeDotsMenu - Ferdig
    
    * OverflowItem - Endret CSS
    
    * Lagt til ThreeDotsMenu i DropdownTab
    
    * Overflow, overflowitem og threedotsmenu - CSS (rem)
    
    * Three Dots Menu, whitespace fix
    
    * DropdownTab - Fully working
    
    * Organizing the OverflowMenu
    
    * Export and rename components
    
    Co-authored-by: oscarsb <oscarsb@stud.ntnu.no>

---
+++ src/components/DropdownTab/index.tsx
@@ -0,0 +1,74 @@
+import React, { FC, useState } from "react";
+import cx from "classnames";
+import styles from "./DropdownTab.module.css";
+import { DefaultProps } from "../../types";
+import ContentTab from "../ContentTab"
+import DropDownArrow from "../icons/DropDownArrow";
+import OverflowMenuIcon from "../icons/OverflowMenuIcon";
+import OverflowItem from "../OverflowMenu/Overflow/OverflowItem"
+import OverflowMenuItem from "../OverflowMenu/Overflow/OverflowItem"
+import ThreeDotsMenu from "../OverflowMenu";
+
+//type ColorType = "green" | "blue" | "purple" | "red" | "yellow";
+
+interface Props extends DefaultProps {
+    color: string;
+    name: string;
+    //hva skal dukke opp dersom man trykker på
+    child?: any; 
+} 
+
+//må vel få laget en funksjon som kan brukes til å endre isOpen til true dersom den blir trykket på 
+
+const DropdownTab: React.FC<Props> = ({
+        color, 
+        name,
+        className
+    }: Props) => {
+        const [toggle, setToggle] = useState(false);
+        const [clickedArrow, setClickedArrow] = useState(false);
+        const mock = ['hei1','hei2', 'hei3','hei4', 'hei5','hei6', 'hei7','hei8', 'hei9','hei10', 'hei11','hei', 'hei','hei', 'hei','hei', 'hei','hei', 'hei','hei', 'hei','hei', 'hei']
+        
+        let Content = "";
+
+        if (toggle) {
+            Content = "showContent"
+        }
+        else {
+            Content = "hideContent"
+        }
+
+        function handleClick() {
+            setToggle(!toggle)
+            setClickedArrow(clickedArrow)
+            
+        }
+    
+        
+        const DropdownTabClass = cx(styles.DropdownTab, className,  
+            {
+                [styles.red]: color == "red",
+                [styles.yellow]: color == "yellow",
+                [styles.blue]: color == "blue",
+                [styles.green]: color =="green",
+                [styles.purple]: color =="purple"
+            }
+        );
+
+        
+    return (
+        <div>
+            <div className={DropdownTabClass} onClick={()=> handleClick()}> 
+                {name}
+                <DropDownArrow clicked={clickedArrow}/>
+            </div>
+            <ContentTab contentTabClass={Content} name={name}>
+                {mock.map(name => <div className={styles.column} key={name}> 
+                    <ThreeDotsMenu/>{name} 
+                </div>)}
+            </ContentTab>
+        </div>
+    );
+};
+
+export default DropdownTab; 
---
+++ src/components/icons/GarbageCan.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from "react";
+
+interface Props {
+  size?: number;
+}
+
+const GarbageCan: FC<Props> = ({size}) => {
+  return (
+    <svg width={size} height={size} viewBox="0 0 10 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0.714286 10.6667C0.714286 11.4 1.35714 12 2.14286 12H7.85714C8.64286 12 9.28571 11.4 9.28571 10.6667V2.66667H0.714286V10.6667ZM10 0.666667H7.5L6.78571 0H3.21429L2.5 0.666667H0V2H10V0.666667Z" fill="black"/>
+    </svg>
+  );
+};
+
+export default GarbageCan;
